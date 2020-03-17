a2afb342d70bb97bb2432a4666962ae5ab1935c8
Henrik Sandberg <henrik.a.sandberg@icloud.com>
2020-03-17 12:20:39 +0100

    Initial Commit

---
+++ Battle Roll/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Battle Roll
+//
+//  Created by Henrik Anthony Odden Sandberg on 17/03/2020.
+//  Copyright © 2020 Henrik Anthony Odden Sandberg. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
